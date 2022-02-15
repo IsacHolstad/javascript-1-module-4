33b654755ab39914c0a4f462625048ab7b11a2bd
IsacHolstad <wisac22@gmail.com>
2022-02-15 15:38:08 +0100

    training filtered function

---
+++ training/script.js
@@ -0,0 +1,12 @@
+//training from the moodle excersises 
+//filter()method 
+//there is something wrong with this code "undefined not a function"
+const numbers = [1, 3, 6, 5, 34, 654, 54, 23, 542, 541, 10, 11, 16, 20];
+
+const filteredNumbers = numbers.filter();
+function filterNumbers(number) {
+    if(number > 19) {
+        return true;
+    }
+    console.log(filterNumbers)
+};
